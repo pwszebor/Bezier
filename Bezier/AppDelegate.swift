621cec86395b9966c3b5f6bc6fa9307b8a851081
pwszebor <p.wszeborowski@stud.elka.pw.edu.pl>
2018-11-17 13:35:16 +0100

    Initial Commit

---
+++ Bezier/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bezier
+//
+//  Created by Paweł Wszeborowski on 17/11/2018.
+//  Copyright © 2018 Paweł Wszeborowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
